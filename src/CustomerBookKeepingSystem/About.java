4fe9850a9daaf914bb63083867bcea31d80eed85
SweetyParajuli <98260926+SweetyParajuli@users.noreply.github.com>
2023-03-20 22:35:39 +0545

    Customer Booking project

---
+++ src/CustomerBookKeepingSystem/About.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CustomerBookKeepingSystem;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class About extends JFrame implements ActionListener {
+
+    JButton b1;
+    JLabel l1;
+    Font f, f1, f2;
+    TextArea t1;
+    String s;
+
+    public About() {
+
+        setLayout(null);
+        JButton b1 = new JButton("Exit");
+        add(b1);
+        b1.setBounds(180, 430, 120, 20);
+        b1.addActionListener(this);
+
+        Font f = new Font("RALEWAY", Font.BOLD, 180);
+        setFont(f);
+
+        s = "                                    About Projects          \n  "
+                + "\nThe objective of the Customer Bookkeeping System"
+                + " is to develop a system that automates the processes "
+                + "and activities of a travel and the purpose is to design a "
+                + "system using which one can perform  operations related to "
+                + "Customer.\n\n"
+                + "This application will help in accessing the information related "
+                + "to the customer \n\n"
+                + "Advantages of Project:"
+                + "\nGives accurate information"
+                + "\nSimplifies the manual work"
+                + "\nIt minimizes the documentation related work"
+                
+                
+               
+                ;
+
+        TextArea t1 = new TextArea(s, 10, 40, Scrollbar.VERTICAL);
+        t1.setEditable(false);
+        t1.setBounds(20, 100, 450, 300);
+
+        add(t1);
+
+        Font f1 = new Font("RALEWAY", Font.BOLD, 16);
+        t1.setFont(f1);
+
+        Container contentPane = this.getContentPane();
+        t1 = new TextArea();
+
+        JLabel l1 = new JLabel("About Project");
+        add(l1);
+        l1.setBounds(170, 10, 180, 80);
+        l1.setForeground(Color.red);
+
+        Font f2 = new Font("RALEWAY", Font.BOLD, 20);
+        l1.setFont(f2);
+
+        setBounds(700, 220, 500, 550);
+
+        setLayout(null);
+        setVisible(true);
+        
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        dispose();
+    }
+
+    public static void main(String args[]) {
+        new About().setVisible(true);
+    }
+
+}
---
+++ src/CustomerBookKeepingSystem/Home.java
@@ -0,0 +1,216 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CustomerBookKeepingSystem;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class Home extends JFrame{
+    String username;
+    public static void main(String[] args) {
+        new Home("").setVisible(true);
+    }
+    
+    public Home(String username) {
+        super("Customer BookKeeping System");
+	this.username = username;
+        setForeground(Color.CYAN);
+        setLayout(null); 
+
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/home.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1950, 1000,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2); 
+	JLabel NewLabel = new JLabel(i3);
+	NewLabel.setBounds(0, 0, 1950, 1000); 
+        add(NewLabel);
+        
+        JLabel l1 = new JLabel("Customer BookKeeping System");
+	l1.setForeground(Color.WHITE);
+        l1.setFont(new Font("Tahoma", Font.BOLD, 55));
+	l1.setBounds(500, 60, 1000, 100);
+	NewLabel.add(l1);
+		
+		
+        JMenuBar menuBar = new JMenuBar();
+	setJMenuBar(menuBar);
+		
+        JMenu m1 = new JMenu("CUSTOMER");
+        m1.setForeground(Color.BLUE);
+          m1.setFont(new Font("Tahoma", Font.PLAIN, 45));
+	m1.setBounds(500, 60, 1000, 100);
+	menuBar.add(m1);
+		
+        JMenuItem mi1 = new JMenuItem("ADD CUSTOMER");
+	m1.add(mi1);
+        
+        JMenuItem mi2 = new JMenuItem("UPDATE CUSTOMER DETAIL");
+	m1.add(mi2);
+        
+
+        
+        mi1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new AddCustomer(username).setVisible(true);
+                }catch(Exception e ){}
+            }
+	});
+        
+        mi2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new UpdateCustomer(username).setVisible(true);
+                }catch(Exception e ){}
+            }
+	});
+        
+       
+        
+       
+	
+        
+        
+		
+	JMenu m2 = new JMenu("PACKAGES");
+        m2.setForeground(Color.RED);
+        m2.setFont(new Font("Tahoma", Font.PLAIN, 45));
+	m2.setBounds(500, 60, 1000, 100);
+        
+	menuBar.add(m2);
+        
+        JMenuItem mi6 = new JMenuItem("CHECK PACKAGE");
+	m2.add(mi6);
+        
+        JMenuItem mi7 = new JMenuItem("BOOK PACKAGE");
+	m2.add(mi7);
+        
+        JMenuItem mi5 = new JMenuItem("VIEW PACKAGE");
+	m2.add(mi5);
+        
+        
+        mi6.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new CheckPackage().setVisible(true);
+                }catch(Exception e ){}
+            }
+	});
+        
+
+        mi7.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new BookPackage(username).setVisible(true);
+                }catch(Exception e ){}
+            }
+	});
+        
+
+	mi5.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new ViewPackage(username).setVisible(true);
+                }catch(Exception e ){}
+                
+            }
+	});
+        
+        
+        JMenu m3 = new JMenu("HOTELS");
+        m3.setForeground(Color.BLUE);
+        m3.setFont(new Font("Tahoma", Font.PLAIN, 45));
+	m3.setBounds(500, 60, 1000, 100);
+	menuBar.add(m3);
+        
+        JMenuItem mi8 = new JMenuItem("BOOK HOTELS");
+	m3.add(mi8);
+        
+        JMenuItem mi9 = new JMenuItem("VIEW HOTELS");
+	m3.add(mi9);
+        
+        JMenuItem mi10 = new JMenuItem("VIEW BOOKED HOTEL");
+	m3.add(mi10);
+        
+        mi8.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                new BookHotel(username).setVisible(true);
+            }
+	});
+        
+        
+        
+	mi9.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new CheckHotels().setVisible(true);
+                }catch(Exception e ){}
+            }
+	});
+        
+        mi10.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    new ViewBookedHotel(username).setVisible(true);
+                }catch(Exception e ){}
+            }
+	});
+        
+        
+        
+        JMenu m6 = new JMenu("UTILITY");
+        m6.setForeground(Color.RED);
+        m6.setFont(new Font("Tahoma", Font.PLAIN, 45));
+	m6.setBounds(500, 60, 1000, 100);
+	menuBar.add(m6);
+        
+        JMenuItem mi13 = new JMenuItem("NOTEPAD");
+	m6.add(mi13);
+        
+        JMenuItem mi14 = new JMenuItem("CALCULATOR");
+	m6.add(mi14);
+        
+        mi13.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    Runtime.getRuntime().exec("notepad.exe");
+                }catch(Exception e){ }
+            }
+	});
+        
+        
+        mi14.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    Runtime.getRuntime().exec("calc.exe");
+                }catch(Exception e){ }
+            }
+	});
+        
+        JMenu m7 = new JMenu("ABOUT");
+        m7.setForeground(Color.BLUE);
+        m7.setFont(new Font("Tahoma", Font.PLAIN, 50));
+	m7.setBounds(500, 60, 1000, 100);
+	menuBar.add(m7);
+        
+        JMenuItem mi15 = new JMenuItem("ABOUT");
+	m7.add(mi15);
+        
+        mi15.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                new About().setVisible(true);
+            }
+	});
+        
+        
+        
+        setExtendedState(JFrame.MAXIMIZED_BOTH); 
+	setVisible(true);
+        getContentPane().setBackground(Color.WHITE);
+    }
+}
